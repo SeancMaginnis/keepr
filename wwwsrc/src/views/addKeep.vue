e6b4128f26b27b4654046154f31d755e5891a31d
seancmaginnis <seancmaginnis@gmail.com>
2019-04-15 17:30:12 -0600

    getting there

--- wwwsrc/src/views/addKeep.vue
+++ wwwsrc/src/views/addKeep.vue
@@ -1,27 +1,30 @@
 <template>
-    <div class="Form">
+    <div class="kForm col">
         <form @submit.prevent="addKeep">
-            <input  type="text" placeholder="Name" v-model="makeKeep.name">
-            <input  type="text" placeholder="Description" v-model="makeKeep.description">
+            <input type="text" placeholder="Name" v-model="makeKeep.name">
+            <input type="text" placeholder="Description" v-model="makeKeep.description">
             <input type="text" placeholder="URL for Image" v-model="makeKeep.img">
+
             <button class="btn btn-outline-danger" type="submit">Create Keep</button>
         </form>
-        
-        
+
+
+
     </div>
 </template>
 
 <script>
     export default {
         name: "addKeep",
-        data(){
-            return{
+        data() {
+            return {
                 makeKeep: {}
             }
         },
-        
-        
-        methods:{
+
+
+
+        methods: {
             addKeep: function () {
                 this.$store.dispatch("createKeep", this.makeKeep)
             }
@@ -30,5 +33,9 @@
 </script>
 
 <style scoped>
-
+    .kForm {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
 </style>
